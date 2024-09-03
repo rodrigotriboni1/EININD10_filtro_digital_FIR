2759778ee8b3cdf050e1b01f08ed31c9b4468b2b
Josué Silva de Morais <josuemorais@gmail.com>
2024-09-03 10:42:46 -0300

    refactor instal python

--- python/00_instalLibPython.bat
+++ python/00_instalLibPython.bat
@@ -1,4 +1,5 @@
-python.exe -m pip install --upgrade pip
+Call python.exe -m pip install --upgrade pip
+Call python.exe -m pip install ipykernel -U --user --force-reinstall
 Call pip install --upgrade nodejs
 Call pip install --upgrade ipympl
 Call pip install --upgrade ipython
@@ -6,7 +7,6 @@ Call pip install --upgrade ipywidgets
 Call pip install --upgrade matplotlib
 Call pip install --upgrade pandas
 Call pip install --upgrade numpy
-Call pip install --upgrade pyats
 Call pip install --upgrade scipy
 Call pip install --upgrade pyserial
 Call pip install --upgrade soundfile
